4aa4d80a5efafce4f128953ccc836b90973d1c5a
racppatricia78GH <r4chelpatriciatan788268@gmail.com>
2026-02-05 17:12:59 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 // Inisialisasi Swiper Foto
 const swiperFoto = new Swiper('.swiper-foto', {
     loop: true,
+    autoplay: {
+    delay: 3500,
+  },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true},
     effect: 'fade', // Memberikan efek transisi halus
     fadeEffect: { crossFade: true },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-});
-
-// Inisialisasi Swiper Teks
-const swiperTeks = new Swiper('.swiper-text', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
     },
 });
